913d1643a20c434acb7565a5900b66230b7551f7
letterarrow <letterarrow@users.noreply.github.com>
2019-09-22 02:06:14 +0900

    implement fan mode as temporary

--- climate.py
+++ climate.py
@@ -1,10 +1,16 @@
 from homeassistant.components.climate import ClimateDevice
 from homeassistant.components.climate.const import (
+    ATTR_FAN_MODE,
     ATTR_HVAC_MODE,
     HVAC_MODE_OFF,
     HVAC_MODE_HEAT,
     HVAC_MODE_COOL,
     HVAC_MODE_DRY,
+    FAN_AUTO,
+    FAN_LOW,
+    FAN_MEDIUM,
+    FAN_HIGH,
+    SUPPORT_FAN_MODE,
     SUPPORT_TARGET_TEMPERATURE
 )
 from homeassistant.const import (
@@ -28,6 +34,7 @@ class MyClimate(ClimateDevice):
         self._name = name
         self._hvac_mode = HVAC_MODE_OFF
         self._target_temp = 25
+        self._fan_mode = FAN_AUTO
 
     @property
     def temperature_unit(self):
@@ -50,19 +57,35 @@ class MyClimate(ClimateDevice):
     def target_temperature(self):
         return self._target_temp
 
+    @property
+    def fan_mode(self):
+        return self._fan_mode
+    
+    @property
+    def fan_modes(self):
+        return [
+            FAN_AUTO,
+            FAN_LOW,
+            FAN_MEDIUM,
+            FAN_HIGH
+        ]
+
     def set_temperature(self, **kwargs):
         self._target_temp = kwargs.get(ATTR_TEMPERATURE)
 
     @property
     def target_temperature_step(self):
         return 1.0
-    
+
+    def set_fan_mode(self, fan_mode):
+        self._fan_mode = fan_mode
+
     def set_hvac_mode(self, hvac_mode):
         self._hvac_mode = hvac_mode
 
     @property
     def supported_features(self):
-        return SUPPORT_TARGET_TEMPERATURE
+        return SUPPORT_TARGET_TEMPERATURE | SUPPORT_FAN_MODE
     
     @property
     def min_temp(self):
